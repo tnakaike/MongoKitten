ff492fe0b3f1cad4530c2907e342ab9c1ba446a5
Takuya Nakaike <nakaike@jp.ibm.com>
2018-04-02 05:41:43 -0400

    updated Package.swift

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ var package = Package(
         ],
     dependencies: [
         // For MongoDB Documents
-        .Package(url: "https://github.com/OpenKitten/BSON.git", versions: Version(5, 2, 0) ..< Version(6, 0, 0)),
+        .Package(url: "https://github.com/tnakaike/BSON.git", versions: Version(5, 2, 4) ..< Version(6, 0, 0)),
         
         // For ExtendedJSON support
         .Package(url: "https://github.com/OpenKitten/Cheetah.git", majorVersion: 2),
 
         // Authentication
-        .Package(url: "https://github.com/OpenKitten/CryptoKitten.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/tnakaike/CryptoKitten.git", majorVersion: 0, minor: 2),
 
         // Asynchronous behaviour
         .Package(url: "https://github.com/OpenKitten/Schrodinger.git", majorVersion: 1),
